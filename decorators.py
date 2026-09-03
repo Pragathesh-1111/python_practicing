a3790ca685a9395e94da3e62a6b4e420c272cf34
Pragatheshwaran <pragatheshwaran2006@gmail.com>
2026-09-03 22:45:25 +0530

    decorators

--- decorators.py
+++ decorators.py
@@ -1,14 +1,33 @@
 def my_decorator(func):
-    def wrap_func():
+    def wrap_func(*args, **kwargs):
         print('Start')
-        func()
+        func(*args, *kwargs)
         print('End')
     return wrap_func
 
 
 @my_decorator
-def hello():
-    print('Helloooooo')
+def hello(greeting, emoji=':('):
+    print(greeting, emoji)
 
 
-hello()
+hello('Hiiiiiiiii', ':)')
+
+from time import time
+
+
+def performance(func):
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f"It took {t2-t1}s")
+    return wrap_func
+
+@performance
+def long_time():
+    for item in range(60000000):
+        item+1
+
+long_time()
+
