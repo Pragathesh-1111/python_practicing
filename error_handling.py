b3522465640a169d582f98d63c946254dd62adf8
Pragatheshwaran <pragatheshwaran2006@gmail.com>
2026-09-05 23:37:00 +0530

    Final of err handling

--- error_handling.py
+++ error_handling.py
@@ -12,3 +12,14 @@ while True:
     else:
         print('Thank you!')
         break
+    
+def multiply(num1, num2):
+    try:
+        1/num1
+        1/num2
+        return num1 * num2
+    except (TypeError, ZeroDivisionError) as err:
+        print(f'Please use Numbers as arguments: {err}')
+    finally:
+        print('Done')
+multiply(0, 5)
